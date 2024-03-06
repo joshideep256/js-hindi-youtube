14e147fde0d9da53e0c4ae857f6029596f2c9e31
Deep Joshi <132056088+joshideep256@users.noreply.github.com>
2024-03-06 18:37:27 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=14453
+let accountEmail="hitesh@gmail.com"
+var sccounrtpassword="12345"
+accountCity="Jaipur"
+
+// accountId=2  // not allowed
+
+accountEmail ="hc@hc.com"
+accountPassword = "21212121"
+accountCity ="Bengaluru"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountCity,accountPassword])
