f43f48754fbaa42a7fd53a5c9438007a1bddd55c
Deep Joshi <132056088+joshideep256@users.noreply.github.com>
2024-03-06 18:49:49 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello")
+
+console.log(3
+    + 
+    3)  //code readablitiy should be high
+
+console.log("hitesh")
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+
+//number => 2 to the power 53
+//bigint
+//string=>""
+//boolean =>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+
+//object
+
+console.log(typeof undefined)  //undefined
+console.log(typeof null)  //object
+
+
+
